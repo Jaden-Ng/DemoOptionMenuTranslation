976683edd0a440778af8e6ca6cd7c5ed29e399ce
Jaden-Ng <20016584@myrp.edu.sg>
2021-05-31 11:27:46 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20016584/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id20016584.demooptionmenutranslation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvTranslatedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTranslatedText=findViewById(R.id.textviewtranslate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.EnglishSelection){
+            tvTranslatedText.setText("Hello");
+            return true;
+        }else if(id==R.id.italianSelection){
+            tvTranslatedText.setText("Ciao");
+            return true;
+        }else{
+            tvTranslatedText.setText("Error translation");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
